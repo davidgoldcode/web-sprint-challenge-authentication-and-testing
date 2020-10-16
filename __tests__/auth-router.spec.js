2334e236560cb3badc2c5adcf01f7ab35c72d786
David Gold <davidgold@hey.com>
2020-10-16 12:48:52 -0400

    mvp met

--- __tests__/auth-router.spec.js
+++ __tests__/auth-router.spec.js
@@ -1,7 +1,10 @@
 const supertest = require("supertest");
 const server = require("../api/server.js");
+const session = require("supertest-session");
 
 describe("Tests for our Auth Router", () => {
+  beforeEach(() => (testSession = session(server)));
+
   describe("Tests for /register POST", () => {
     it("should return 200 OK", () => {
       const fakeUser = {
@@ -51,6 +54,34 @@ describe("Tests for our Auth Router", () => {
         .send(credentials)
         .then((res) => {
           expect(res.type).toMatch(/json/i);
+        });
+    });
+  });
+
+  describe("test for /joke GET request", () => {
+    const credentials = {
+      username: "dg",
+      password: "dg123",
+    };
+
+    it("should return 200 OK", () => {
+      return testSession
+        .post("/api/auth/login")
+        .send(credentials)
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it("should return 401 with wrong credentials", () => {
+      return testSession
+        .post("/api/auth/login")
+        .send({
+          username: "fake",
+          password: "notreal",
+        })
+        .then((res) => {
+          expect(res.status).toBe(401);
         });
     });
   });
